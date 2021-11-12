640baef435407d780c86c4bc23ccecc6b565a10f
jhony <jhony@DESKTOP-VCR4V9E>
2021-11-12 10:17:50 +0100

    Version 1

---
+++ src/src/Cuenta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author jhony
+ */
+public class Cuenta {
+    private int saldo;
+    private long accNum;
+    public Cuenta(int saldo, long num)
+    {
+    }
+    public int getBalance()
+    {
+        return 0;
+    }
+    public void depositar(int cantidad)
+    {
+    }
+    public void retirar(int cantidad)
+    {
+    }
+}
